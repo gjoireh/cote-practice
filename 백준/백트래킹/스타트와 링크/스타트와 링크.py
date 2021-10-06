e35005f81577900fabe4bcbe8bb96a9804aa21b0
user <gjrwgeo@gmail.com>
2021-10-06 15:38:55 +0900

    스타트와 링크 브루트 포스, 백트래킹

---
+++ 백준/백트래킹/스타트와 링크/스타트와 링크.py
@@ -0,0 +1,57 @@
+import sys
+
+N = 0
+S = []
+team1 = []
+team2 = []
+diffs = []
+
+
+def back_tracking(t1, t2):
+    if len(t1) > N // 2 or len(t2) > N // 2:
+        return True
+    return False
+
+
+def save_diff(t1, t2):
+    sum_of_t1 = 0
+    sum_of_t2 = 0
+    for i in range(N // 2):
+        for j in range(N // 2):
+            if i == j:
+                continue
+            sum_of_t1 += S[t1[i]][t1[j]]
+            sum_of_t2 += S[t2[i]][t2[j]]
+    diff = abs(sum_of_t1 - sum_of_t2)
+    diffs.append(diff)
+    return
+
+
+def make_team(index, t1, t2):
+    if index == N:
+        if len(t1) == N // 2 and len(t2) == N // 2:
+            return True
+        else:
+            return False
+
+    if back_tracking(t1, t2):
+        return False
+
+    t1.append(index)
+    if make_team(index + 1, t1, t2):
+        save_diff(t1, t2)
+    t1.pop()
+
+    t2.append(index)
+    if make_team(index + 1, t1, t2):
+        save_diff(t1, t2)
+    t2.pop()
+
+
+if __name__ == "__main__":
+    N = int(input())
+    for i in range(N):
+        S.append(list(map(int, sys.stdin.readline().split())))
+
+    make_team(0, team1, team2)
+    print(min(diffs))
