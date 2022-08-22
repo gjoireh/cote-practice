2dbf1182ece38fc42a48843fc4e6af0c93058592
user <gjrwgeo@gmail.com>
2022-08-22 16:53:53 +0900

    완전탐색 최소직사각형

---
+++ 프로그래머스/LV1/완전탐색/최소직사각형/최소직사각형.py
@@ -0,0 +1,12 @@
+def solution(sizes):
+    answer = 0
+    w = []
+    h = []
+    
+    for s in sizes:
+        w.append(max(s))
+        h.append(min(s))
+    
+    answer = max(w) * max(h)
+            
+    return answer
