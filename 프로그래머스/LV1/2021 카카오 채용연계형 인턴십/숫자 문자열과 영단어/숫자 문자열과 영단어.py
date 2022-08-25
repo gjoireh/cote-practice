7b9bf7ade9eb7e1a1d360e043c09554aff51cdc4
user <gjrwgeo@gmail.com>
2022-08-25 20:32:40 +0900

    2021 카카오 채용연계형 인턴십 숫자 문자열과 영단어

---
+++ 프로그래머스/LV1/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,22 @@
+numbers = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+number_word_dic = {
+    "zero" : "0",
+    "one" : "1",
+    "two" : "2",
+    "three" : "3",
+    "four" : "4",
+    "five" : "5",
+    "six" : "6",
+    "seven" : "7",
+    "eight" : "8",
+    "nine" : "9",
+}
+def solution(s):
+    answer = 0
+    for i in range(10):
+        s = s.replace(numbers[i], number_word_dic[numbers[i]])
+    answer = int(s)
+    return answer
+
+
+print(solution("one4seveneight"))
