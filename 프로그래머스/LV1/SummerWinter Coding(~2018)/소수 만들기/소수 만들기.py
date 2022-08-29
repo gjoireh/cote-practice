e6a9cbe249d1951ffb1f055f78d7854da7e75a76
user <gjrwgeo@gmail.com>
2022-08-30 00:46:08 +0900

    SummerWinter Coding(~2018) 소수 만들기.py

---
+++ 프로그래머스/LV1/SummerWinter Coding(~2018)/소수 만들기/소수 만들기.py
@@ -0,0 +1,33 @@
+import math
+
+answer = 0
+prime_numbers = set()
+used = set()
+
+
+def is_prime_number(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
+def go(j, cnt, n, m, nums):
+    global answer
+    if cnt == m:
+        if is_prime_number(sum(used)):
+            answer += 1
+        return
+
+    for i in range(j, n):
+        if nums[i] in used:
+            continue
+        used.add(nums[i])
+        go(i+1, cnt + 1, n, m, nums)
+        used.remove(nums[i])
+
+
+def solution(nums):
+    global answer
+    go(0, 0, len(nums), 3, nums)
+    return answer
