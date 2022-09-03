22cab1bf466da7b4bb578558a9c530f0e97af61d
user <gjrwgeo@gmail.com>
2022-09-03 10:18:22 +0900

    2019 KAKAO BLIND RECRUITMENT 실패율

---
+++ 프로그래머스/LV1/2019 KAKAO BLIND RECRUITMENT/실패율/실패율.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def solution(N, stages):
+    answer = []
+    sn_fr_dict = {x: 0 for x in range(1,N+1)}
+
+    np_stages = np.array(stages)
+    d = len(stages)
+    for n in range(1,N+1):
+        if n not in stages:
+            sn_fr_dict[n] = 0
+        else:
+            u = len(np.where(np_stages==n)[0])
+            sn_fr_dict[n] = u / d
+            d -= u
+
+    reversed_by_value = sorted(sn_fr_dict, key=lambda x: sn_fr_dict[x], reverse=True)
+
+    answer = reversed_by_value
+
+    return answer
+
+# def solution(N, stages):
+# .
+# .
+# .
+#     for n in range(1,N+1):
+#         if n not in stages:
+#             sn_fr_dict[n] = 0
+#         else:
+#             sn_fr_dict[n] = len(np.where(np_stages==n)[0]) / len(np.where(np_stages>=n)[0])
+# .
+# .
+# .
+#     return answer
