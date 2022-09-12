ce4deaa39451b9607c5a67f5ab2c64348923cb76
user <gjrwgeo@gmail.com>
2022-09-12 09:14:53 +0900

    월간 코드 챌린지 시즌1 내적 코드 수정

--- 프로그래머스/LV1/월간 코드 챌린지 시즌1/내적/내적.py
+++ 프로그래머스/LV1/월간 코드 챌린지 시즌1/내적/내적.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def solution(a, b):
-    l1 = np.array(a)
-    l2 = np.array(b)
-    answer = np.multiply(l1,l2).tolist()
-    return sum(answer)
+    v1 = np.array(a)
+    v2 = np.array(b)
+    v3 = v1*v2
+    return int(sum(v3))
