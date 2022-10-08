2ae0d67955c815dab13ef59824b090d5db47c8cf
user <gjrwgeo@gmail.com>
2022-10-08 17:22:11 +0900

    코딩테스트 입문 평행.c

--- 프로그래머스/LV0/코딩테스트 입문/평행/평행.c
+++ 프로그래머스/LV0/코딩테스트 입문/평행/평행.c
@@ -2,11 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int l1[2][2] = { {0,0}, {0,0} };
-int l2[2][2] = { {0,0}, {0,0} };
-int sub1[2] = { 0,0 };
-int sub2[2] = { 0,0 };
-int idx_of_l1 = 0;
+struct dot
+{
+    int x;
+    int y;
+}typedef DOT;
+
+DOT l1[2];
+DOT l2[2];
+DOT sub1;
+DOT sub2;
+
 int idx_of_l2 = 0;
 bool group[4] = { false, false, false, false };
 
@@ -22,36 +28,35 @@ int solution(int** dots, size_t dots_rows, size_t dots_cols)
             group[i] = true;
             group[j] = true;
 
-            l1[idx_of_l1][0] = dots[i][0];
-            l1[idx_of_l1++][1] = dots[i][1];
-            l1[idx_of_l1][0] = dots[j][0];
-            l1[idx_of_l1][1] = dots[j][1];
+            l1[0].x = dots[i][0];
+            l1[0].y = dots[i][1];
+            l1[1].x = dots[j][0];
+            l1[1].y = dots[j][1];
 
             for (int k = 0; k < 4; k++)
             {
                 if (!group[k])
                 {
-                    l2[idx_of_l2][0] = dots[k][0];
-                    l2[idx_of_l2++][1] = dots[k][1];
+                    l2[idx_of_l2].x = dots[k][0];
+                    l2[idx_of_l2++].y = dots[k][1];
                 }
             }
 
-            idx_of_l1 = 0;
             idx_of_l2 = 0;
             group[i] = false;
             group[j] = false;
 
-            sub1[0] = l1[0][0] - l1[1][0];
-            sub1[1] = l1[0][1] - l1[1][1];
-            sub2[0] = l2[0][0] - l2[1][0];
-            sub2[1] = l2[0][1] - l2[1][1];
+            sub1.x = l1[0].x - l1[1].x;
+            sub1.y = l1[0].y - l1[1].y;
+            sub2.x = l2[0].x - l2[1].x;
+            sub2.y = l2[0].y - l2[1].y;
 
-            if ((sub1[0] == 0 && sub2[0] != 0) || (sub1[0] != 0 && sub2[0] == 0))
+            if ((sub1.x == 0 && sub2.x != 0) || (sub1.x != 0 && sub2.x == 0))
                 continue;
 
-            if ((sub1[0] == 0 && sub2[0] == 0) ||
-                (sub1[1] == 0 && sub2[1] == 0) ||
-                ((float)sub1[1] / (float)sub1[0]) == (float)(sub2[1] / (float)sub2[0]))
+            if ((sub1.x == 0 && sub2.x == 0) ||
+                (sub1.y == 0 && sub2.y == 0) ||
+                ((float)sub1.y / (float)sub1.x) == (float)(sub2.y / (float)sub2.x))
                 return 1;
         }
     }
