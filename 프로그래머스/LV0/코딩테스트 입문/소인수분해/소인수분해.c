8958387d6212bd6da1339ca0515bcbe464bfefc3
user <gjrwgeo@gmail.com>
2022-10-27 11:51:20 +0900

    코딩테스트 입문 소인수 분해

--- 프로그래머스/LV0/코딩테스트 입문/소인수분해/소인수분해.c
+++ 프로그래머스/LV0/코딩테스트 입문/소인수분해/소인수분해.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
+int* solution(int n) 
+{
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* ans = (int*)malloc(n);
+    int ans_idx = 0;
+    int i = 2;
+    while(n > 1)
+    {
+        if(n % i == 0)
+        {
+            ans[ans_idx++] = i;
+            while (n % i == 0)
+            {
+                n /= i;
+            }
+        }
+        i++;
+    }
+    return ans;
+}
+
+/*
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <math.h>
 
 struct prime_numbers_and_len
@@ -50,3 +76,4 @@ int* solution(int n)
     }
     return answer;
 }
+*/
