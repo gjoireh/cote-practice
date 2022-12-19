129dab366186849540cc57711cfdf2347f7e88c4
user <gjrwgeo@gmail.com>
2022-12-19 21:34:49 +0900

    코딩테스트 연습 2021 KAKAO BLIND RECRUITMENT : 순위 검색

--- 프로그래머스/LV2/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/순위 검색.py
+++ 프로그래머스/LV2/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/순위 검색.py
@@ -1,24 +1,21 @@
 from bisect import bisect_left
 
 def get_all_querys(src, src_idx, query, all_querys):  
-
     if src_idx == 4:
-        all_querys["".join(query)] = []
+        all_querys[query] = []
         return 
 
     for v in src[src_idx]:
-        query.append(v)
-        get_all_querys(src, src_idx+1, query, all_querys)
-        query.pop()
+        get_all_querys(src, src_idx+1, query+v, all_querys)
         
 def solution(info, query):
     ans = []
     all_querys = dict()
     src = [['cpp', 'java', 'python', '-'], ['frontend', 'backend', '-'], ['junior', 'senior', '-'], ['chicken', 'pizza', '-']]
     infos = [["".join(info[:-1]), int(info[-1])] for info in [data.split(' ') for data in info]]
-    querys = [["".join(query[:-1]), int(query[-1])] for query in [data.replace("and ", '').split(" ") for data in query]]
+    querys = [[query[0], int(query[-1])] for query in [data.replace(" and ", '').split(" ") for data in query]]
 
-    get_all_querys(src, 0, [], all_querys)
+    get_all_querys(src, 0, "", all_querys)
 
     for k in list(all_querys.keys()):
         for info in infos:
