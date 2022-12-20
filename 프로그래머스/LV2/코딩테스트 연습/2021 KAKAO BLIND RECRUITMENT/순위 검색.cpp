918752aa8bb3a14a4a043b764f2a682b7feffced
user <gjrwgeo@gmail.com>
2022-12-20 10:12:12 +0900

    코딩테스트 연습 2021 KAKAO BLIND RECRUITMENT : 순위 검색

--- 프로그래머스/LV2/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/순위 검색.cpp
+++ 프로그래머스/LV2/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/순위 검색.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 const string ALL = "-";
-unordered_map<string, vector<int>> all_queries_derived_from_info;
+unordered_map<string, vector<int>> queries_derived_from_info;
 
-void get_all_queries_derived_from_info(string* input_info, int all_mask, int score) {
+void get_queries_derived_from_info(string* input_info, int all_mask, int score) {
     string q = "";
     for (int i = 0; i < 4; i++)
         q += (all_mask & (1 << i)) ? ALL : input_info[i];
-    all_queries_derived_from_info[q].push_back(score);
+    queries_derived_from_info[q].push_back(score);
 }
 
 vector<int> solution(vector<string> info, vector<string> query) {
@@ -24,17 +24,17 @@ vector<int> solution(vector<string> info, vector<string> query) {
     for (auto& inf : info) {
         istringstream iss(inf);
         iss >> input_info[0] >> input_info[1] >> input_info[2] >> input_info[3] >> score;
-        for (int i = 0; i < 16; i++) get_all_queries_derived_from_info(input_info, i, score);
+        for (int i = 0; i < 16; i++) get_queries_derived_from_info(input_info, i, score);
     }
 
-    for (auto& aqdfi : all_queries_derived_from_info) sort(aqdfi.second.begin(), aqdfi.second.end());
+    for (auto& aqdfi : queries_derived_from_info) sort(aqdfi.second.begin(), aqdfi.second.end());
 
     for (auto& que : query) {
         istringstream iss(que);
         iss >> input_info[0] >> tmp >> input_info[1] >> tmp >> input_info[2] >> tmp >> input_info[3] >> score;
         string q = input_info[0] + input_info[1] + input_info[2] + input_info[3];
         
-        answer.push_back(all_queries_derived_from_info[q].end() - lower_bound(all_queries_derived_from_info[q].begin(), all_queries_derived_from_info[q].end(), score));
+        answer.push_back(queries_derived_from_info[q].end() - lower_bound(queries_derived_from_info[q].begin(), queries_derived_from_info[q].end(), score));
     }
     return answer;
 }
